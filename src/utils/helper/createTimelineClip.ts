c7cb763423a1fc329eac0fa757487aef3eaab524
Viktor Starodubtsev <viktor_one@icloud.com>
2026-05-24 20:19:55 +0500

    implemented file createTimelineClip.ts for factory function

---
+++ src/utils/helper/createTimelineClip.ts
@@ -0,0 +1,40 @@
+import type { TimelineClip } from "../types";
+
+interface CreateClipProps {
+    assetId: string;
+  start: number;
+  duration: number;
+  type: TimelineClip["type"];
+  groupId?: string;
+}
+
+
+export function createTimelineClip({
+  assetId,
+  start,
+  duration,
+  type,
+  groupId,
+}: CreateClipProps): TimelineClip {
+  return {
+    id: crypto.randomUUID(),
+    assetId,
+    start,
+    duration,
+    type,
+
+    name: "unknown",
+
+    groupId,
+
+    sourceOffset: 0,
+
+    x: 50,
+    y: 50,
+    width: 200,
+    height: 200,
+
+    fadeIn: type === "audio" ? 1 : 0,
+    fadeOut: type === "audio" ? 1 : 0,
+  };
+}
