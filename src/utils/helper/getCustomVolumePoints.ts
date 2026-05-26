0a9f795e0d54b1b8fe6620b62217786d257c5bd5
Viktor Starodubtsev <viktor_one@icloud.com>
2026-05-26 16:44:16 +0500

    added context for getCustomVolumePoints.ts

---
+++ src/utils/helper/getCustomVolumePoints.ts
@@ -0,0 +1,18 @@
+import type { TimelineClip } from "../types";
+
+ interface Props{
+    clip: TimelineClip
+    scale: number;
+ }
+
+
+ export function getCustomVolumePoints({clip, scale}: Props){
+    const H = 40
+
+    const points = (clip.volumePoints ?? []).map((point) => ({
+        x: point.time * scale,
+        y: H - point.value * H
+    }))
+
+    return points
+ }
