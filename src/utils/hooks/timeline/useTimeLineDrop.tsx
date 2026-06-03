8514b0c54b808066da3cd80c7aa1142f5b167690
Viktor Starodubtsev <viktor_one@icloud.com>
2026-06-03 18:52:23 +0500

    added new function for useTimeLIneDrop handleDragOver and added state gostClip

--- src/utils/hooks/timeline/useTimeLineDrop.tsx
+++ src/utils/hooks/timeline/useTimeLineDrop.tsx
@@ -1,4 +1,4 @@
-import { useCallback, type DragEvent } from "react";
+import { useCallback, useState, type DragEvent } from "react";
 import { useClips } from "../../../context";
 import { useFileReader } from "../ui/useFileReader";
 import { useAddAssetToTimeline } from "../../helper/useAssetToTimeline";
@@ -7,10 +7,19 @@ interface Props {
   scale: number;
 }
 
+type GostClip = {
+  trackId: string;
+  start: number;
+  duration: number;
+  type: string;
+};
+
 export function useTimeLineDrop({ scale }: Props) {
   const { clips, tracks, addClip } = useClips();
   const { readFile } = useFileReader();
   const addAssetToTimeline = useAddAssetToTimeline();
+
+  const [gostClip, setGostClip] = useState<GostClip | null>(null);
 
   const handleDrop = useCallback(
     async (e: DragEvent<HTMLDivElement>) => {
@@ -63,5 +72,51 @@ export function useTimeLineDrop({ scale }: Props) {
     [addAssetToTimeline, addClip, clips, readFile, tracks, scale],
   );
 
-  return { handleDrop };
+  const handleDragOver = useCallback(
+    (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left + e.currentTarget.scrollLeft;
+      const start = Math.max(0, Math.round((x / scale) * 10) / 10);
+      const rows = document.querySelectorAll(".track_row");
+
+      let targetTrack = null;
+
+      for (let element = 0; element < rows.length; element++) {
+        const rowRect = rows[element].getBoundingClientRect();
+
+        if (e.clientY >= rowRect.top && e.clientY <= rowRect.bottom) {
+          targetTrack = tracks[element];
+          break;
+        }
+      }
+
+      if (!targetTrack) {
+        setGostClip(null);
+        return;
+      }
+
+      const clipID = e.dataTransfer.getData("clipId");
+      const asset = clips.find((item) => item.id === clipID);
+
+      if (!asset) {
+        return;
+      }
+
+      const newGostClip: GostClip = {
+        trackId: targetTrack.id,
+        start,
+        duration: asset.duration,
+        type: asset.type,
+      };
+
+      setGostClip({
+        ...newGostClip,
+      });
+    },
+    [scale, clips, tracks],
+  );
+
+  return { handleDrop, handleDragOver, gostClip };
 }
