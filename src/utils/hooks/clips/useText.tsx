78d6ac29f66369faf4b9b8ce791e6901baba5561
Viktor Starodubtsev <viktor_one@icloud.com>
2026-06-06 17:20:16 +0500

    added new hook useText for addedText

---
+++ src/utils/hooks/clips/useText.tsx
@@ -0,0 +1,35 @@
+import { useClips, useCurrentTime } from "../../../context";
+import { useCallback } from "react";
+import { createClipsFromRegistry } from "../../helper/createClipsFromRegistry";
+import type { TextAsset } from "../..";
+
+export function useText() {
+  const { addClip, addToTrack, tracks } = useClips();
+  const { currentTime } = useCurrentTime();
+
+  const handleAddedText = useCallback(() => {
+    const targetTrack = tracks[0];
+
+    if (!targetTrack) return;
+
+    const asset: TextAsset = {
+      id: crypto.randomUUID(),
+      type: "text",
+      text: "New Text",
+      size: 5,
+      duration: 5,
+      name: "Text",
+    };
+
+    addClip(asset);
+
+    createClipsFromRegistry({
+      asset,
+      type: "text",
+      start: currentTime,
+      targetTrack,
+      addToTrack,
+    });
+  }, [addClip, addToTrack, currentTime, tracks]);
+  return { handleAddedText };
+}
